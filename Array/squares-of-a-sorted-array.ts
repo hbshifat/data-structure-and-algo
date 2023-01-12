536a5ab1993e0b4413d7d6e14a62e4f34dbf0253
hbshifat <hbshifat30@gmail.com>
2023-01-12 17:43:18 +0600

    Added: Array Section

---
+++ Array/squares-of-a-sorted-array.ts
@@ -0,0 +1,10 @@
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in non-decreasing order.
